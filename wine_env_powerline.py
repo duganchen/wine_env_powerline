4b6f5425a217a79046093eefe9437190f1d6f56b
Dugan Chen <thedoogster@gmail.com>
2014-12-27 22:13:35 -0800

    virtualenv corrected to bottle

--- wine_env_powerline.py
+++ wine_env_powerline.py
@@ -1,2 +1,6 @@
-def bottle(pl):
-    return "Bottle Shown"
+from powerline.theme import requires_segment_info
+
+@requires_segment_info
+def bottle(pl, segment_info):
+    return segment_info['environ'].get('BOTTLE') or None
+
